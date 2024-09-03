54cc9d7f75d9141ed23bf0ef59ebcff6320a906e
Bjornar <bjornar.schinke@etu.umontpellier.fr>
2024-09-03 14:11:14 +0200

    Debut
    Created Class CommandeRemplacer.java / added function "executer" - Added "remplacer" to Switch in CommandeFactory.java
    
    Added the option to rempace a section with empty text

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,27 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument
+{
+    public CommandeRemplacer(Document document, String[] paramteres)
+    {
+        super(document, paramteres);
+    }
+
+    public void executer() {
+        String texte = "";
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : remplacer;debut;fin;texte");
+            return;
+        }
+        if (parameters.length >= 4) {
+            texte = parameters[3];
+        }
+
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        this.document.remplacer(debut, fin, texte);
+        super.executer();
+    }
+}

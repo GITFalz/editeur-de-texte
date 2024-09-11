913ff45858b2a36689a4c777478a1ee6a6a1adff
Bjornar <bjornar.schinke@etu.umontpellier.fr>
2024-09-11 09:26:10 +0200

    feat: added documentation

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,7 +1,13 @@
 package fr.iut.editeur.document;
 
+/**
+ * Class that stores the text
+ */
 public class Document {
 
+    /**
+     * text variable
+     */
     private String texte;
 
     public Document() {
@@ -16,16 +22,31 @@ public class Document {
         this.texte = texte;
     }
 
+    /**
+     * Adds text at the end of the current text
+     * @param texte the text to add
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /**
+     * Replace a part of the text with another
+     * @param debut the start of the zone in number of chars
+     * @param fin the end of the zone in number of chars
+     * @param remplacement the text to replace the zone with
+     */
     public void remplacer(int debut, int fin, String remplacement) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    /**
+     * Set all letters in the zone to upper case
+     * @param debut start of the zone in number of chars
+     * @param fin end of the zone in number of chars
+     */
     public void majuscules(int debut, int fin) {
         String partieGauche = texte.substring(0, debut);
         String minuscule = texte.substring(debut, fin + 1);
@@ -39,10 +60,18 @@ public class Document {
         texte = partieGauche + majuscule + partieDroite;
     }
 
+    /**
+     * Erase a zone in the text
+     * @param debut start of the zone in number of chars
+     * @param fin end of the zone in number of chars
+     */
     public void effacer(int debut, int fin) {
         remplacer(debut, fin, "");
     }
 
+    /**
+     * Clear the text
+     */
     public void clear() {
         this.texte = "";
     }

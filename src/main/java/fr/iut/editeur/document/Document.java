f31645d6e556305a21d40dff082ae3f52d01dbc3
Bjornar Schinkel <bjornar.schinkel@etu.umontpellier.fr>
2024-09-11 09:10:07 +0200

    feat: added documentation

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,7 +1,13 @@
 package fr.iut.editeur.document;
 
+/**
+ * Classe ou le texte est sauvegarder
+ */
 public class Document {
 
+    /**
+     * Texte sauvegarder
+     */
     private String texte;
 
     public Document() {
@@ -25,11 +31,23 @@ public class Document {
         return this.texte;
     }
 
+    /**
+     * Remplacer une partie de texte de debut a fin par remplacement
+     * @param debut debut de le region à mettre en majuscules en nombre de char
+     * @param fin fin de le region à mettre en majuscules en nombre de char
+     * @param remplacement
+     */
     public void remplacer(int debut, int fin, String remplacement) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
+
+    /**
+     * Metter en majuscule une partie du texte de debut a fin
+     * @param debut debut de le region à mettre en majuscules en nombre de char
+     * @param fin fin de le region à mettre en majuscules en nombre de char
+     */
     public void majuscules(int debut, int fin) {
         //TO-DO!
         String texteMaj = texte.substring(debut,fin+1);
@@ -37,13 +55,28 @@ public class Document {
         remplacer(debut,fin,texteMaj);
 
     }
+
+    /**
+     * Efface un region du texte de debut à fin
+     * @param debut debut de la zone de texte en nombre de char
+     * @param fin fin de la zone de texte en nombre de char
+     */
     public void effacer (int debut,int fin){
         remplacer(debut,fin,"");
     }
+
+    /**
+     * Efface le texte
+     */
     public void clear (){
         texte = "";
     }
 
+    /**
+     * Insere du texte à index
+     * @param index place dans la zone de texte en nomnbre de char
+     * @param texte nouveau texte à ajouter
+     */
     public void inserer(int index, String texte) {
         remplacer(index+1, index+1, texte);
     }
